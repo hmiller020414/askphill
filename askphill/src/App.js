d800d6ad916d7b017c08f5e5e7e7747875bdd63d
hmiller020414 <hayleymm14@gmail.com>
2022-01-26 09:46:43 +0100

    install sass, update data to include stock information

--- askphill/src/App.js
+++ askphill/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import MenuWrapper from './components/MenuWrapper'
 
 function App() {
--- askphill/src/data/MenuData.js
+++ askphill/src/data/MenuData.js
@@ -33,33 +33,84 @@ const DropdownData = [
     {
         title: 'Daily',
         options: [
-            `Men's Daily`,
-            `Women's Daily`,
-            `Kid's Daily`,
-            `See All Daily`
+            {
+                name: "Men's Daily",
+                stock: "14",
+            },
+            {
+                name: "Women's Daily",
+                stock: "33",
+            },
+            {
+                name: "Kid's Daily",
+                stock: "10",
+            },
+            {
+                name: "See All Daily",
+                stock: "57",
+            }
         ],
     },
     {
         title: 'Sport',
         options: [
-            `Men's Sport`,
-            `Women's Sport`,
-            `Kid's Sport`,
-            `Recover`,
-            `Run`,
-            `Ski`,
-            `Walk`,
-            `Weight Lift`,
-            `See All Sport`
+            {
+                name: "Men's Sport",
+                stock: "22",
+            },
+            {
+                name: "Women's Sport",
+                stock: "22",
+            },
+            {
+                name: "Kid's Sport",
+                stock: "12",
+            },
+            {
+                name: "Recover",
+                stock: "4",
+            },
+            {
+                name: "Run",
+                stock: "9",
+            },
+            {
+                name: "Ski",
+                stock: "5",
+            },
+            {
+                name: "Walk",
+                stock: "15",
+            },
+            {
+                name: "Weight Lift",
+                stock: "20",
+            },
+            {
+                name: "See All Sport",
+                stock: "56",
+            }
         ]
     },
     {
         title: 'Travel',
         options: [
-            `Men's Travel`,
-            `Women's Travel`,
-            `Kid's Travel`,
-            `See All Travel`
+            {
+                name: "Men's Travel",
+                stock: "8",
+            },
+            {
+                name: "Women's Travel",
+                stock: "8",
+            },
+            {
+                name: "Kid's Travel",
+                stock: "4",
+            },
+            {
+                name: "See All Travel",
+                stock: "20",
+            }
         ]
     }
 ]

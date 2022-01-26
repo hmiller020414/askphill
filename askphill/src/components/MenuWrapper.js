ca3ac24ed69e7adf461a511e3fb04b2b30b746e3
hmiller020414 <hayleymm14@gmail.com>
2022-01-26 10:15:04 +0100

    initial menu wrapper and header styles

--- askphill/src/components/MenuWrapper.js
+++ askphill/src/components/MenuWrapper.js
@@ -2,14 +2,12 @@ import MenuData from '../data/MenuData'
 import Header from './header/Header'
 import SliderWrapper from './slider/SliderWrapper'
 import DropdownWrapper from './dropdown/DropdownWrapper'
+import './MenuWrapper.scss'
 
 const MenuWrapper = () => {
 
-    console.log(MenuData.MenuSlideData)
-
     return (
         <div className="MenuWrapper">
-            <h1>Here is my header</h1>
             <Header />
             <h1>Here is my slider</h1>
             <SliderWrapper />
--- askphill/src/components/header/Header.js
+++ askphill/src/components/header/Header.js
@@ -1,11 +1,21 @@
 import ShoppingBag from '../../images/ShoppingBag.svg'
 import Stox_Logo from '../../images/Stox_Logo.png'
+import './Header.scss'
 
 const Header = () => {
     return (
         <header className="Header">
-            <img src={Stox_Logo} alt='Stox Logo' />
-            <img src={ShoppingBag} alt='Shopping Bag' />
+            <div className="Header-Left">
+                <button className="Hamburger">
+                    <div className="Hamburger-Line"></div>
+                    <div className="Hamburger-Line"></div>
+                    <div className="Hamburger-Line"></div>
+                </button>
+                <img src={Stox_Logo} className="StoxLogo" alt='Stox Logo' />
+            </div>
+            <div className="Header-Right">
+                <img src={ShoppingBag} className="ShoppingBag" alt='Shopping Bag' />
+            </div>
         </header>
     )
 }
